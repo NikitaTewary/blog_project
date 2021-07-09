df30582b17a537b8d16b07fc0d6ab805316b4abb
NIKITA TEWARY <62873522+NikitaTewary@users.noreply.github.com>
2021-07-09 13:06:55 +0530

    added HTML Files and updated views and url files

--- blog_project/mysite/blog/views.py
+++ blog_project/mysite/blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect_field_name
+from django.utils import timezone
 from blog.models import Post,Comment
 from blog.forms import PostForm,CommentForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (TemplateView,ListView,
                                  DetailView,CreateView,UpdateView,
@@ -42,3 +44,38 @@ class DraftListView(LoginRequiredMixin,ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date_isnull=True).order_by('created_date')
+
+@login_required
+def post_publish(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    post.publish
+    return redirect('post_detail',pk=pk)
+
+@login_required
+def add_comment_to_post(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    if request.method=='Post':
+        form=CommentForm(request.POST)
+        if form.is_valid():
+            comment=form.save(commit=False)
+            comment.post=post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form=CommentForm()
+    return render(request,'blog/comment_form.html',{'form':form})
+
+
+
+@login_required
+def comment_appove(request,pk):
+    comment=get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment=get_object_or_404(Comment,pk=pk)
+    post_pk=comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
